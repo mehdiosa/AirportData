24cfb9c97bdde8029f6da959950a6f0e136f9d95
Osama Mehdi <mehdi@hm.edu>
2023-09-01 16:25:41 +0200

    Fixup testing to work with github actions

--- .github/scripts/test_app.sh
+++ .github/scripts/test_app.sh
@@ -4,7 +4,6 @@
 set -eo pipefail
 
 xcodebuild -workspace AirportDataApp.xcworkspace \
-            -scheme AirportData\
-            -destination platform=iOS\ Simulator,OS=16.2,name=iPhone\ 14 \
+            -scheme "AirportData" \
+            -destination generic/platform=iOS \
             clean test
-            
