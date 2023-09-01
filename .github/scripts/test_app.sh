bc27000f4465fc3a2a4d17c68541724fdfbdb869
Osama Mehdi <mehdi@hm.edu>
2023-09-01 16:12:54 +0200

    - add testing to workflow - add simple test for AirportData for checking purposes

---
+++ .github/scripts/test_app.sh
@@ -0,0 +1,9 @@
+
+#!/bin/bash
+
+set -eo pipefail
+
+xcodebuild -workspace AirportDataApp.xcworkspace \
+            -scheme AirportData\
+            -destination platform=iOS\ Simulator,OS=16.2,name=iPhone\ 14 \
+            clean test
