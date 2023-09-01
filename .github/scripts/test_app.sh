8a22e5347626b950d90a07be01a32b0a04a707c3
Osama Mehdi <mehdi@hm.edu>
2023-09-01 16:22:14 +0200

    set execute permission for script

--- .github/scripts/test_app.sh
+++ .github/scripts/test_app.sh
@@ -7,3 +7,4 @@ xcodebuild -workspace AirportDataApp.xcworkspace \
             -scheme AirportData\
             -destination platform=iOS\ Simulator,OS=16.2,name=iPhone\ 14 \
             clean test
+            
