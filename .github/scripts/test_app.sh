3dbaa0c3d022873f9c37fdecaeae7f484c1e415c
Osama Mehdi <mehdi@hm.edu>
2023-09-01 16:28:39 +0200

    - restore exact device information for testing

--- .github/scripts/test_app.sh
+++ .github/scripts/test_app.sh
@@ -4,6 +4,6 @@
 set -eo pipefail
 
 xcodebuild -workspace AirportDataApp.xcworkspace \
-            -scheme "AirportData" \
-            -destination generic/platform=iOS \
+            -scheme AirportData\
+            -destination platform=iOS\ Simulator,OS=16.2,name=iPhone\ 14 \
             clean test
